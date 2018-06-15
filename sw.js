acc13162d5275b663defe63143c2c909220f782a
Myungjin Kim <weumj1@gmail.com>
2018-06-16 01:24:20 +0900

    ServiceWorker - network, offline

--- sw.js
+++ sw.js
@@ -2,6 +2,8 @@
 
 const carDealsCacheName = "carDealsCacheV1";
 const carDealsCachePagesName = "carDealsCachePagesV1";
+const carDealsCacheImagesName = "carDealsCacheImagesV1";
+
 const carDealsCacheFiles = [
   "js/app.js",
   "js/carService.js",
@@ -26,6 +28,12 @@ const carDealsCacheFiles = [
   "resources/systemjs/system.src.js"
 ];
 
+const resourcePath = {
+  latest: "/pluralsight/courses/progressive-web-apps/service/latest-deals.php",
+  image: "/pluralsight/courses/progressive-web-apps/service/car-image.php",
+  car: "/pluralsight/courses/progressive-web-apps/service/car.php"
+};
+
 self.addEventListener("install", event => {
   console.log('From SW: Install Event"', event);
   self.skipWaiting();
@@ -46,13 +54,66 @@ self.addEventListener("activate", event => {
     (async () => {
       const cacheKeys = await caches.keys();
 
+      const cacheNames = [
+        carDealsCacheName,
+        carDealsCachePagesName,
+        carDealsCacheImagesName
+      ];
       const deleting = cacheKeys
-        .filter(
-          item => item !== carDealsCacheName && item !== carDealsCachePagesName
-        )
+        .filter(item => !cacheNames.includes(item))
         .map(caches.delete.bind(caches));
 
-      await Promise.all(deleting);
+      Promise.all(deleting);
     })()
   );
 });
+
+self.addEventListener("fetch", e => {
+  const requestUrl = new URL(e.request.url);
+  const { pathname: requestPath } = requestUrl;
+  const fileName = requestPath.substring(requestPath.lastIndexOf("/"));
+
+  if (requestPath === resourcePath.latest || fileName === "sw.js") {
+    e.respondWith(fetch(e.request));
+  } else if (requestPath === resourcePath.image) {
+    e.respondWith(networkFirstStrategy(e.request));
+  } else {
+    e.respondWith(cacheFirstStrategy(e.request));
+  }
+});
+
+const getCacheName = request => {
+  const requestUrl = new URL(request.url);
+  const { pathName: requestPath } = requestUrl;
+
+  switch (requestPath) {
+    case resourcePath.image:
+      return carDealsCacheImagesName;
+    case resourcePath.car:
+      return carDealsCachePagesName;
+    default:
+      return carDealsCacheName;
+  }
+};
+const fetchRequestAndCache = async request => {
+  const networkResponse = await fetch(request);
+  caches
+    .open(getCacheName(request))
+    .then(cache => cache.put(request, networkResponse));
+
+  return networkResponse.clone();
+};
+
+const cacheFirstStrategy = async request => {
+  const cacheResponse = await caches.match(request);
+
+  return cacheResponse || fetchRequestAndCache(request);
+};
+
+const networkFirstStrategy = async request => {
+  try {
+    return await fetchRequestAndCache(request);
+  } catch (e) {
+    return caches.match(request);
+  }
+};
