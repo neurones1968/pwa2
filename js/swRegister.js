abbc77e2754315612492a915f427b2a6a6fde308
Myungjin Kim <weumj1@gmail.com>
2018-06-16 00:15:40 +0900

    ServiceWorker - lifecycle

--- js/swRegister.js
+++ js/swRegister.js
@@ -5,8 +5,24 @@
 
   try {
     const swRegistration = await navigator.serviceWorker.register("sw.js");
+    let serviceWorker;
 
-    console.log(swRegistration);
+    if (swRegistration.installing) {
+      console.log("Resolved at installing: ", swRegistration);
+      serviceWorker = swRegistration.installing;
+    } else if (swRegistration.waiting) {
+      console.log("Resolved at installed/waiting: ", swRegistration);
+      serviceWorker = swRegistration.waiting;
+    } else if (swRegistration.active) {
+      console.log("Resolved at activated: ", swRegistration);
+      serviceWorker = swRegistration.active;
+    }
+
+    if (serviceWorker) {
+      serviceWorker.addEventListener("statechange", e => {
+        console.log(e.target.state);
+      });
+    }
   } catch (e) {
     console.warn("ServiceWorker registration has failed.", e);
   }
